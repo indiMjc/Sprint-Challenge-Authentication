d9ba5ceffda5dca92987cd820df56b051c52b381
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-20 11:32:59 -0500

    register user works

--- users/users-model.js
+++ users/users-model.js
@@ -2,11 +2,19 @@ const db = require('../database/dbConfig');
 
 module.exports = {
   add,
-  findBy
+  findBy,
+  findById
 };
+
+function findById(id) {
+  return db('users').where({ id });
+  first();
+}
 
 async function add(user) {
   const [id] = await db('users').insert(user);
+
+  return findById(id);
 }
 
 function findBy(filter) {
