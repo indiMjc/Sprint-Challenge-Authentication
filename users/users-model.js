f27690cd856dd104be11a588af637520719f797f
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-20 11:35:10 -0500

    both login and register return user that was added as a single object

--- users/users-model.js
+++ users/users-model.js
@@ -7,14 +7,15 @@ module.exports = {
 };
 
 function findById(id) {
-  return db('users').where({ id });
-  first();
+  return db('users')
+    .where({ id })
+    .first();
 }
 
 async function add(user) {
   const [id] = await db('users').insert(user);
 
-  return findById(id);
+  return findById(id).first();
 }
 
 function findBy(filter) {
