7fe985670daa919714c99a0a65edb9b27894827c
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-20 11:27:01 -0500

    register middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/users-model');
+
+const signToken = user => {
+  const payload = {
+    username: user.username
+  };
+
+  const secret = process.env.JWT_SECRET;
+
+  const options = {
+    expiresIn: '1h'
+  };
+
+  return jwt.sign(payload, secret, options);
+};
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 4);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 router.post('/login', (req, res) => {
